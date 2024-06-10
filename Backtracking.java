59de149f139a1e67db634833cc349a89df62b078
agusrypstra <agusrypstra@gmail.com>
2024-06-09 23:43:18 -0300

    Se le añaden explicaciones a los algoritmos

--- Backtracking.java
+++ Backtracking.java
@@ -1,5 +1,18 @@
 package TPE;
 
+/*
+Funcionamiento de la clase Backtracking:
+- Primero hereda la clase EstrategiaAlgoritmica con:
+    - Atributos:
+        - Arreglo de Tareas
+        - Arreglo de Procesadores
+        - Arreglo de Asignaciones
+        - Metrica de costo
+        - Maximo sin refrigeracio
+    - Metodos:
+        -
+
+ */
 import java.util.ArrayList;
 
 public class Backtracking extends EstrategiaAlgoritmica{
@@ -9,15 +22,14 @@ public class Backtracking extends EstrategiaAlgoritmica{
         super(tareas, procesadores,maximoSinRefrigeracion);
         mejorAsignacion = new ArrayList<>();
         mejorTiempoTotal = Integer.MAX_VALUE;
-        ArrayList<Asignacion> asignaciones = new ArrayList<>();
     }
     public void asignar() {
         ArrayList<Asignacion> asignaciones = new ArrayList<>();
         backtrack(0,asignaciones);
         super.imprimirAsignaciones(mejorAsignacion);
     }
+    /*El algoritmo de backtracking va a tener una complejidad computacional de O(T^P . P). Este peor caso se daria*/
     private void backtrack(int tareaActual, ArrayList<Asignacion> asignaciones){
-        metricaDeCosto++;
         if (tareaActual == super.getTareas().size()) {
             int tiempoTotalActual = calcularTiempoMaximo(asignaciones);
             if (tiempoTotalActual < mejorTiempoTotal) {
@@ -28,7 +40,8 @@ public class Backtracking extends EstrategiaAlgoritmica{
         }
         Tarea tarea = super.getTareas().get(tareaActual);
         for (Procesador procesador : super.getProcesadores()) {
-            if (super.esAsignable(tarea, procesador)) {
+            if (super.esAsignable(tarea, procesador,asignaciones)) {
+                metricaDeCosto++;
                 asignaciones.add(new Asignacion(procesador, tarea));
                 backtrack(tareaActual + 1, asignaciones);
                 asignaciones.remove(asignaciones.size() - 1);
