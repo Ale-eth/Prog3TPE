c52d09090b7976c4f4d8800543b201396b95cc0c
agusrypstra <agusrypstra@gmail.com>
2024-06-09 23:45:56 -0300

    Nuevas pruebas en el main

--- Main.java
+++ Main.java
@@ -6,13 +6,13 @@ public class Main {
         public static void main(String args[]) {
 //            Servicios servicios = new Servicios("./src/tpe/datasets/Procesadores.csv", "./src/tpe/datasets/Tareas.csv");
             Servicios servicios = new Servicios("datasets/Procesadores.csv", "datasets/Tareas.csv");
-//            System.out.println(servicios.servicio1("T2"));
-//            System.out.println(servicios.servicio2(false));
-//            System.out.println(servicios.servicio3(0,10000));
+            System.out.println(servicios.servicio1("T2"));
+            System.out.println(servicios.servicio2(false));
+            System.out.println(servicios.servicio3(25,50));
 
-            //Integer tiempoMaximo, HashMap<String, Procesador> procesadores
-            Backtracking b = new Backtracking(servicios.getTareasList(),servicios.getProcesadoresList());
-            b.resolverAsignaciones(400);
+            Backtracking b = new Backtracking(servicios.getTareasList(),servicios.getProcesadoresList(),500);
+            b.asignar();
+            Greedy g = new Greedy(servicios.getTareasList(),servicios.getProcesadoresList(),500);
+            g.asignar();
         }
     }
-
