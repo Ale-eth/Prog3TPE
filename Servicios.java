e5075284b22bf141dcf2fc5840dd8d178d5c7d2f
agusrypstra <agusrypstra@gmail.com>
2024-06-09 23:45:20 -0300

    Se especifican las complejidades computacionales de los servicios

--- Servicios.java
+++ Servicios.java
@@ -4,7 +4,6 @@ import TPE.utils.CSVReader;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.IllegalFormatCodePointException;
 import java.util.List;
 
 // NO modificar la interfaz de esta clase ni sus métodos públicos.
@@ -21,29 +20,29 @@ public class Servicios {
         tareasTree = new Tree();
         tareasHashMap = new HashMap<>();
         procesadoresHashMap = new HashMap<>();
-
         CSVReader reader = new CSVReader();
         reader.readProcessors(pathProcesadores, procesadoresHashMap);
         reader.readTasks(pathTareas,tareasHashMap,tareasTree);
     }
-
     public ArrayList<Tarea> getTareasList(){
         return new ArrayList<>(this.tareasHashMap.values());
     }
     public ArrayList<Procesador> getProcesadoresList(){
         return new ArrayList<>(this.procesadoresHashMap.values());
     }
+
     // Expresar la complejidad temporal del servicio 1.
-    // Servicio 1: Dado un identificador de tarea obtener toda la información de la tarea asociada.
+    // Servicio 1: Dado un identificador de tarea obtener toda la información de la tarea asociada.+
+    // El servicio 1 tiene una complejidad computacional de o(1) por que usamos una estructura HashMap para almacenar
+    // las tareas, en un HashMap el acceso por clave tiene complejidad computacional o(1)
     public Tarea servicio1(String ID) {
         return tareasHashMap.get(ID);
     }
-    // El servicio 1 tiene una complejidad computacional de o(1) por que usamos una estructura HashMap para almacenar
-    // las tareas, en un HashMap el acceso por clave tiene complejidad computacional o(1)
-
 
     // Expresar la complejidad temporal del servicio 2.
     // Servicio 2: Permitir que el usuario decida si quiere ver todas las tareas críticas o no críticas y generar el listado apropiado resultante.
+    // El servicio 2 tiene una complejidad o(n) ya que debe recorrer toda la lista de tareas para comprobar
+    // las que cumplen el criterio de si es critica o no.
     public List<Tarea> servicio2(boolean esCritica) {
         ArrayList<Tarea> salida = new ArrayList<>();
         for (Tarea t: tareasHashMap.values()) {
@@ -53,16 +52,16 @@ public class Servicios {
         }
         return salida;
     }
-    // El servicio 2 tiene una complejidad o(n) ya que debe recorrer toda la lista de tareas para comprobar
-    // las que cumplen el criterio de si es critica o no.
+
 
 
     // Expresar la complejidad temporal del servicio 3.
     // Servicio 3: Obtener todas las tareas entre 2 niveles de prioridad indicados.
+    // El servicio 3 es o(n) donde n es la cantidad de tareas, ya que en el peor de los casos se debera recorrer el arbol entero.
+    //Se utiliza un arbol binario de usqueda para tener las tareas ordenadas por el parametro que deseamos y asi ahorrar complejidad.
     public List<Tarea> servicio3(int prioridadInferior, int prioridadSuperior) {
         return tareasTree.getTareasEntreNivelDePrioridad(prioridadInferior, prioridadSuperior);
     }
-    // El servicio 3 es o(n) donde n es la cantidad de tareas
 
     public HashMap<String, Procesador> getProcesadoresHashMap() {
         HashMap<String, Procesador> copia = new HashMap(procesadoresHashMap);
@@ -72,5 +71,8 @@ public class Servicios {
     public HashMap<String, Tarea> getTareasHashMap() {
         HashMap<String, Tarea> copia = new HashMap(tareasHashMap);
         return copia;
+    }
+    public Tree getTareasTree(){
+        return tareasTree.copy();
     }
 }
